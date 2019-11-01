d442c03f4d20cfaddc476617704f3f10564101fe
Mamadou AMADOU <doumstyle@gmail.com>
2019-11-01 09:55:24 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Welcome to my Portfolio</h1>
+    <p>Coming soon...</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'home'
+
+}
+</script>
